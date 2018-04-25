d7e44b0abb85b80d4fe788cf3dfec91951a974c3
Alex Bernal <csrbernal609@gmail.com>
2018-04-25 18:53:43 -0500

    fix minors

--- paraview/run_pv.py
+++ paraview/run_pv.py
@@ -13,36 +13,40 @@ import docker
 def run_pclient(displayVar, user, group, home, username):
     imageName = "al3x609/paraview:5.5"
     containerName = "pview-" + username
-    print displayVar
     pclient = docker.from_env()
 
-    pclient.containers.run(
-        user=user,
-        detach=True,
-        auto_remove=True,
-        image=imageName,
-        runtime="nvidia",
-        working_dir=home,
-        name=containerName,
-        hostname=containerName,
-        environment=[
-            "NVIDIA_DRIVER_CAPABILITIES=all",
-            "NVIDIA_VISIBLE_DEVICES=all",
-            "USER=" + username,
-            "DISPLAY=:" + displayVar,
-            "VGL_LOGO=1",
-            "QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb",
-            "LIBGL_DEBUG=verbose"
-        ],
-        volumes={
-            home: {'bind': home, 'mode': 'rw'},
-            '/etc/group': {'bind': '/etc/group', 'mode': 'ro'},
-            '/etc/passwd': {'bind': '/etc/passwd', 'mode': 'ro'},
-            '/etc/sudoers.d': {'bind': '/etc/sudoers.d', 'mode': 'ro'},
-            '/tmp/.X11-unix': {'bind': '/tmp/.X11-unix', 'mode': 'rw'},
-            home + '/.Xauthority': {'bind': home + '/.Xauthority', 'mode': 'ro'}
-        }
-    )
+    try:
+        pclient.containers.run(
+            user=str(user) + ":" + str(group),
+            detach=True,
+            auto_remove=True,
+            image=imageName,
+            runtime="nvidia",
+            working_dir=home,
+            name=containerName,
+            hostname=containerName,
+            environment=[
+                "NVIDIA_DRIVER_CAPABILITIES=all",
+                "NVIDIA_VISIBLE_DEVICES=all",
+                "QT_X11_NO_MITSHM=1",
+                "USER=" + username,
+                "DISPLAY=:" + displayVar,
+                "VGL_LOGO=1",
+                "QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb",
+                "LIBGL_DEBUG=verbose"
+            ],
+            volumes={
+                home: {'bind': home, 'mode': 'rw'},
+                '/etc/group': {'bind': '/etc/group', 'mode': 'ro'},
+                '/etc/shadow': {'bind': '/etc/shadow', 'mode': 'ro'},
+                '/etc/passwd': {'bind': '/etc/passwd', 'mode': 'ro'},
+                '/etc/sudoers.d': {'bind': '/etc/sudoers.d', 'mode': 'ro'},
+                '/tmp/.X11-unix': {'bind': '/tmp/.X11-unix', 'mode': 'rw'},                
+            }
+        )
+    except Exception as e:
+        raise e
+
     return pclient
 
 
