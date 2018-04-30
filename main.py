a9ce27b47a195c5ac47df0230e0981934b4b938f
Alex Bernal <csrbernal609@gmail.com>
2018-04-30 14:56:58 -0500

    Update main
    add args options for configure containers

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import run_nvnc as nv
 import run_pv as pv
 import run_pv_server as spv
 from getpass import getuser
+from optparse import OptionParser
 
 
 class RcSC3(object):
 
     __display = None
-    __listen = None
+    __listen_proxy = None
+    __pv_server_port = None
 
     def __init__(self, username, user, group, home):
         self.user = user
@@ -27,11 +29,17 @@ class RcSC3(object):
     def get_display(self):
         return self.__display
 
-    def __set_listen(self, listen_port):
-        self.__listen = listen_port
+    def __set_server_port(self, server_port):
+        self.__pv_server_port = server_port
 
-    def get_listen(self):
-        return self.__listen
+    def get_server_port(self):
+        return self.__pv_server_port
+
+    def __set_listen_proxy(self, listen_port):
+        self.__listen_proxy = listen_port
+
+    def get_listen_proxy(self):
+        return self.__listen_proxy
 
     def start_vnc(self):
         port = 1
@@ -45,14 +53,14 @@ class RcSC3(object):
             cmd = "/opt/TurboVNC/bin/vncserver :" + \
                 str(port) + " -name " + name + " -noxstartup -fg -nohttpd -interframe -mt -nthreads 4"
             subprocess.call(cmd, shell=True)
-            self.__set_display(port + 5900)
+            self.__set_display(display_port=port + 5900)
             return True
         else:
             return False
 
-    def listen_port(self):
+    def free_port(self, port_base):
         # verifica que el puerto este libre 0: <ocupado> 1: <libre>
-        port = 6080
+        port = port_base
         cmd = "netstat -ltn | grep -qs ':" + str(port) + " .*LISTEN' &&  echo '' "
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
         output, ret = p.communicate()[0], p.returncode
@@ -63,78 +71,94 @@ class RcSC3(object):
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
             output, ret = p.communicate()[0], p.returncode
 
-        self.__set_listen(port)
+        return port
+        # self.__set_listen(port)  # return port for proxy novnc
+
+    def port_server_paraview(self):
+        self.__set_server_port(server_port=self.free_port(port_base=11111))
+
+    def port_proxy_nvnc(self):
+        self.__set_listen_proxy(listen_port=self.free_port(port_base=6080))
 
     def __clear(self):
         cmd = "/opt/TurboVNC/bin/vncserver -kill :" + str(self.get_display())
         subprocess.call(cmd, shell=True)
 
-    def menu(self, parallel,n_gpu, np):
-        try:
-            if(parallel):
-                server_paraview = spv.run_ps_client(
-                    displayVar=str(self.get_display() - 5900),
-                    user=self.user,
-                    group=self.group,
-                    home=self.home,
-                    username=self.username,
-                    server_port=11111,
-                    np=np,
-                    n_gpu=n_gpu
-                )
+    def services(self, parallel, np_mpi, n_gpu):
+        self.port_server_paraview()
 
-            client_paraview = pv.run_pclient(
+        if(parallel):
+            server_paraview = spv.run_ps_client(
                 displayVar=str(self.get_display() - 5900),
-                username=self.username,
                 user=self.user,
                 group=self.group,
                 home=self.home,
-                server_port=11111,
-                paralelo=parallel
-            )
-
-            str_tmp = "192.168.66.25:" + str(self.get_display())
-            cmd_vnc = ["--vnc", str_tmp]
-
-            client_nvnc = nv.run_nclient(
-                listen_port=self.get_listen(),
-                cmd=cmd_vnc,
                 username=self.username,
-                user=self.user,
-                group=self.group,
-                home=self.home
-
+                server_port=self.get_server_port(),
+                np=np_mpi,
+                n_gpu=n_gpu
             )
 
-        except Exception as e:
-            raise
+        client_paraview = pv.run_pclient(
+            displayVar=str(self.get_display() - 5900),
+            username=self.username,
+            user=self.user,
+            group=self.group,
+            home=self.home,
+            server_port=self.get_server_port(),
+            paralelo=parallel
+        )
+
+        str_tmp = "192.168.66.25:" + str(self.get_display())
+        cmd_vnc = ["--vnc", str_tmp]
+        self.port_proxy_nvnc()
+        client_nvnc = nv.run_nclient(
+            listen_port=self.get_listen_proxy(),
+            cmd=cmd_vnc,
+            username=self.username,
+            user=self.user,
+            group=self.group,
+            home=self.home
+        )
+
+    def menu(self):
 
         print ("")
         print ("******************************************************************")
         print ("Bienvenido {}".format(self.username))
         print ("DISPLAY asignada: {}".format(self.get_display()))
-        print ("puerto para el tunel {}".format(self.get_listen()))
+        print ("puerto para el tunel {}".format(self.get_listen_proxy()))
         print ("en una terminal ingrese el siguiente comando: ")
-        print ("ssh -qlNT {} -L 15000:ngrid:{} toctoc.sc3.uis.edu.co".format(self.username, self.get_listen()))
+        print ("ssh -qlNT {} -L 15000:ngrid:{} toctoc.sc3.uis.edu.co".format(self.username, self.get_listen_proxy()))
         print ("ingrese a la siguiente URL en su buscador http://127.0.0.1:15000")
         print ("------------------------------------------------------------------")
 
 
 if __name__ == "__main__":
 
+    parser = OptionParser()
+    parser.add_option('-p', '--parallel', dest='parallel', action='store_true',
+                      default=False,
+                      help='select parallel mode paraview (default False)')
+    parser.add_option('-n', '--nprocess', dest='np', action='store',
+                      type="int", default=1,
+                      help='set proccess number mpi (default 1)')
+    parser.add_option('-g', '--n_gpu', dest='ngpu', action='store',
+                      type="int", default=1,
+                      help='set the gpu num to connect to (default 1 of 2)')
+    (options, args) = parser.parse_args()
+
     c = RcSC3(username=getuser(), user=os.getuid(), group=os.getgid(), home=os.getenv('HOME'))
-    print(" user:{} Iduser:{} GdUser:{} home:{}".format(c.username, c.user, c.group, c.home))
-#    try:
+
     subprocess.call("xhost + > /dev/null 2>&1", shell=True)
 
     if(c.start_vnc()):
-        print(" vnc iniciado en disiplay :{}".format(c.get_display()))
-        c.listen_port()
-        print(" proxy escucha en :{}".format(c.get_listen()))
-        c.menu(parallel=True, n_gpu=1, np=1)
+
+        c.port_proxy_nvnc()  # set port novnc proxy
+        c.services(parallel=options.parallel, np_mpi=options.np,
+                   n_gpu=options.ngpu)  # start services by args main
+        c.menu()  # show info to conexion
     else:
-        print(" No hay recursos disponibles")
+        print(" We no have any free service")
+
     subprocess.call("xhost - > /dev/null 2>&1", shell=True)
-#   except Exception:
-#    print (" Find a bug in somewhere! :| ")
-    # c.clear(display)
